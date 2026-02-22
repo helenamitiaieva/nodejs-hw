99b78a9bf37977abb42253a48aec24e088196e07
Helena Mitiaieva <h0636641547@gmail.com>
2026-02-22 15:45:31 +0100

    fixed middleware/errorHandler.js

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,7 +1,10 @@
-export function errorHandler(err, _req, res, _next) {
-  const status = err.status || 500;
+import createHttpError from 'http-errors';
 
-  res.status(status).json({
-    message: err.message,
-  });
-}
+export const errorHandler = (err, req, res, next) => {
+  const isHttpError = err instanceof createHttpError.HttpError;
+
+  const status = isHttpError ? err.status : 500;
+  const message = err.message || 'Internal Server Error';
+
+  res.status(status).json({ message });
+};
